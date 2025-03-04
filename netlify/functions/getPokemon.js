0c029c331c2577dd5c3df01dce02b6581b3af6ac
bellTurt1e <30904963+bellTurt1e@users.noreply.github.com>
2025-03-03 21:26:58 -0800

    Added PokemonTCG API to project and now hosting project with Netlify

---
+++ netlify/functions/getPokemon.js
@@ -0,0 +1,29 @@
+exports.handler = async function (event, context) {
+    const apiKey = process.env.POKEMON_KEY; // Read from environment variables
+    const pokemonNumber = event.queryStringParameters.number;
+
+    if (!pokemonNumber) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "No Pokémon number provided." }),
+        };
+    }
+
+    try {
+        const response = await fetch(`https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:${pokemonNumber}`, {
+            headers: { 'X-Api-Key': apiKey }
+        });
+
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to fetch data" }),
+        };
+    }
+};
